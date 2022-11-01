b15dfa81ea4193d668eaf14252adb51e1c5bbf57
manish <manishghalme1997@gmail.com>
2022-11-01 18:33:13 +0530

    this code use to write the swagger annotation and demo

---
+++ DemoSwaggerAppp/src/main/java/com/DemoSawggwer/DemoSwaggerApppApplication.java
@@ -0,0 +1,13 @@
+package com.DemoSawggwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSwaggerApppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSwaggerApppApplication.class, args);
+	}
+
+}
---
+++ DemoSwaggerAppp/src/test/java/com/DemoSawggwer/DemoSwaggerApppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DemoSawggwer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSwaggerApppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
